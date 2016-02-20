458644a3d66155d77116baa621bf599e6b95d955
David Liebman <radiodee1@gmail.com>
2016-02-20 11:53:18 -0500

    add some buttons for later

--- src/main/java/org/davidliebman/tag/ATAGShowImage.java
+++ src/main/java/org/davidliebman/tag/ATAGShowImage.java
@@ -27,7 +27,11 @@ public class ATAGShowImage {
     private JButton buttonCsvSecond;
     private JButton buttonDBLocal;
     private JButton buttonCsvLocal;
-
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JPanel buttonBar;
 
 
     private ATAG var;
