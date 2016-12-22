08653b6f4d83e752fe4ccc58c46edc26a06ae7cb
David Liebman <radiodee1@gmail.com>
2016-12-22 13:32:45 -0500

    combine two models for saving of results

--- test_python_atag/nn_model.py
+++ test_python_atag/nn_model.py
@@ -137,87 +137,10 @@ class NN(object):
 
 
 
-    '''
-    def skintone_setup(self):
-        input_num = 4 * 3
-        mid_num = 3
-        output_num = 2
-
-        x = tf.placeholder(tf.float32, [None, input_num])
-        W_1 = tf.Variable(tf.random_normal([input_num, mid_num], stddev=0.5))
-        b_1 = tf.Variable(tf.random_normal([mid_num], stddev=0.5))
-
-        y_mid = tf.nn.relu(tf.matmul(x,W_1) + b_1)
-
-        W_2 = tf.Variable(tf.random_normal([mid_num, output_num], stddev=0.5))
-        b_2 = tf.Variable(tf.random_normal([output_num],stddev=0.5))
-
-        y_logits = tf.matmul(y_mid, W_2) + b_2
-        y = tf.nn.softmax(y_logits)
-        #y = tf.nn.softmax(tf.matmul(y_mid, W_2) + b_2)
-
-        y_ = tf.placeholder(tf.float32, [None, output_num])
-
-        cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y_logits, y_))
-
-        train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy) #0.5
-        #train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy) #0.5
-
-        init = tf.initialize_all_variables()
-        #init = tf.global_variables_initializer()
-        #self.sess = tf.Session()
-        self.sess.run(init)
-
-        #summary_writer = tf.train.SummaryWriter(self.ckpt_folder + os.sep + "logs" + os.sep, self.sess.graph)
-
-        if self.load_ckpt : self.load()
-
-        if self.train :
-            self.cursor = 0
-            for i in range(1,self.cursor_tot): #1000
-                batch_xs, batch_ys = self.get_nn_next_train(self.batchsize, 12)
-                self.sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
-
-        if self.save_ckpt and self.train : self.save()
-
-        if self.test :
-            correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
-            accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-
-            #print self.sess.run(y_)
-
-            if self.use_loader : self.get_nn_next_test(self.batchsize, 12)
-            print(self.sess.run(accuracy, feed_dict={x: self.mnist_test.images, y_: self.mnist_test.labels}))
-    '''
-
     def softmax_setup(self):
         input_num = 784 * 3 # like mnist but with three channels
         mid_num = 50 # 10
         output_num = 2
-
-        '''
-        if not self.group_initialize :
-            x = tf.placeholder(tf.float32, [None, input_num])
-            W_1 = tf.Variable(tf.random_normal([input_num, mid_num], stddev=0.0004)) #0.0004
-            b_1 = tf.Variable(tf.random_normal([mid_num], stddev=0.5))
-
-            #y_mid = tf.nn.relu(tf.matmul(x,W_1) + b_1)
-            y_mid = tf.nn.relu(tf.matmul(x,W_1) + b_1)
-
-            W_2 = tf.Variable(tf.random_normal([mid_num, output_num],stddev=0.0004))
-            b_2 = tf.Variable(tf.random_normal([output_num],stddev=0.5))
-
-            y_logits = tf.matmul(y_mid, W_2) + b_2
-            y = tf.nn.softmax(y_logits)
-
-            y_ = tf.placeholder(tf.float32, [None, output_num])
-
-            #cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
-            cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y_logits, y_))
-
-            train_step = tf.train.GradientDescentOptimizer(0.0001).minimize(cross_entropy) #0.0001
-            #train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy) #0.5
-        '''
 
         init = tf.initialize_all_variables()
         #init = tf.global_variables_initializer()
@@ -267,64 +190,7 @@ class NN(object):
 
 
     def conv_setup(self):
-        c_output = 2
-
-        '''
-        if not self.group_initialize :
-            def weight_variable(shape):
-                initial = tf.truncated_normal(shape, stddev=0.1)
-                return tf.Variable(initial)
-
-            def bias_variable(shape):
-                initial = tf.constant(0.1, shape=shape)
-                return tf.Variable(initial)
-
-            def conv2d(x, W):
-                return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
-
-            def max_pool_2x2(x):
-                return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
-                                      strides=[1, 2, 2, 1], padding='SAME')
-
-
-            c_x = tf.placeholder(tf.float32, shape=[None, 784])
-            c_y_ = tf.placeholder(tf.float32, shape=[None, c_output])
-
-
-            self.sess = tf.InteractiveSession()
-            W_conv1 = weight_variable([5, 5, 1, 32])
-            b_conv1 = bias_variable([32])
-            x_image = tf.reshape(c_x, [-1, 28, 28, 1])
-            h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
-            h_pool1 = max_pool_2x2(h_conv1)
-
-            W_conv2 = weight_variable([5, 5, 32, 64])
-            b_conv2 = bias_variable([64])
-
-            h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
-            h_pool2 = max_pool_2x2(h_conv2)
-
-            W_fc1 = weight_variable([7 * 7 * 64, 1024])
-            b_fc1 = bias_variable([1024])
-
-            h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])
-            h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
-
-            keep_prob = tf.placeholder(tf.float32)
-            h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
-
-            W_fc2 = weight_variable([1024, c_output])
-            b_fc2 = bias_variable([c_output])
-
-            y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
-
-
-            c_cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y_conv, c_y_))
-            c_train_step = tf.train.AdamOptimizer(1e-4).minimize(c_cross_entropy)
-            c_correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(c_y_, 1))
-            c_accuracy = tf.reduce_mean(tf.cast(c_correct_prediction, tf.float32))
-
-        '''
+        #c_output = 2
 
         #init = tf.global_variables_initializer()
         init = tf.initialize_all_variables()
