b186825fb4fb88831880c5379896150c0b85f53e
David Liebman <david.c.liebman@gmail.com>
2017-07-27 08:52:11 -0400

    copy and rename

---
+++ test_python_atag/tensorflow/core/user_ops/test/atag_csv.py
@@ -0,0 +1,35 @@
+
+
+class Enum(object) :
+    def __init__(self):
+        self.TEMPLATE_ID = 0
+        self.SUBJECT_ID = 1
+        self.FILE = 2
+        self.MEDIA_ID = 3
+        self.SIGHTING_ID = 4
+        self.FRAME = 5
+        self.FACE_X = 6
+        self.FACE_Y = 7
+        self.FACE_WIDTH = 8
+        self.FACE_HEIGHT = 9
+
+        # only in non-detection csv files
+        self.FACE_YAW = 16
+
+        self.COLOR = 10 # THIS IS MY COLOR
+        self.IS_FACE = 11 # THIS IS MY BOOBLEAN
+        self.ATAG_ID = 12
+        self.TOTAL = 13
+        self.TOTAL_READ = 10
+
+        self.RED = "RED"
+        self.GREEN = "GREEN"
+        self.BLUE = "BLUE"
+
+        self.CONST_ONE_CHANNEL = 1
+        self.CONST_THREE_CHANNEL = 3
+        self.CONST_DOT = 12
+
+        self.AGGREGATE_START = -1
+        self.AGGREGATE_TOUCHED = -5
+        self.AGGREGATE_DELETE = -99
---
+++ test_python_atag/tensorflow/core/user_ops/test/atag_csv_draw_mod.py
@@ -0,0 +1,104 @@
+import os
+import atag_csv as enum
+#import nn_dim as dim
+#import nn_loader as loader
+#from PIL import Image, ImageFile
+
+'''
+Here we read the csv file that we made and train the models
+'''
+
+class Read( enum.Enum, dim.Dimension) :
+    def __init__(self, atag):
+        enum.Enum.__init__(self)
+        dim.Dimension.__init__(self)
+
+
+        self.boxlist_r = []
+        self.boxlist_g = []
+        self.boxlist_b = []
+        self.num = 0
+
+        self.num_chosen = 0
+        self.line_chosen = []
+        self.dim_x = self.DIMENSIONS[self.key][self.COLUMN_XY_CONV][0]
+        self.dim_y = self.DIMENSIONS[self.key][self.COLUMN_XY_CONV][1]
+        self.img = None
+        self.image_folder = atag.VAR_ROOT_DATABASE
+
+
+        self.a = atag
+        self.picname = atag.VAR_IMAGE_NAME
+        self.folder = atag.VAR_ROOT_DATABASE
+
+        if self.picname.startswith(self.folder) :
+            self.picname = self.picname[len(self.folder)+1 : ]
+        print self.picname, "picname"
+
+        self.csv_input = atag.VAR_LOCAL_DATABASE + os.sep + atag.VAR_MY_CSV_NAME + ".csv"
+        self.csv_input_predict = atag.VAR_LOCAL_DATABASE + os.sep + "predict" + ".csv"
+        self.csv_input_dot = atag.VAR_LOCAL_DATABASE + os.sep + atag.VAR_MY_CSV_NAME + ".dot.csv"
+        self.csv_input_predict_list = atag.VAR_LOCAL_DATABASE + os.sep + "predict-list" + ".csv"
+
+    def process_read_line(self, line):
+        line = line.split(",")
+        #print line[self.FILE], "read"
+        if line[self.FILE].endswith(self.picname) :
+            self.num = self.num + 1
+
+            if line[self.COLOR] == self.RED :
+                self.boxlist_r.append([int(line[self.FACE_X]), int(line[self.FACE_Y]),
+                                 int (line[self.FACE_WIDTH]) , int (line[self.FACE_HEIGHT])])
+            if line[self.COLOR] == self.GREEN:
+                self.boxlist_g.append([int(line[self.FACE_X]), int(line[self.FACE_Y]),
+                                       int(line[self.FACE_WIDTH]), int(line[self.FACE_HEIGHT])])
+            if line[self.COLOR] == self.BLUE:
+                self.boxlist_b.append([int(line[self.FACE_X]), int(line[self.FACE_Y]),
+                                       int(line[self.FACE_WIDTH]), int(line[self.FACE_HEIGHT])])
+            if self.num == self.num_chosen:
+                self.line_chosen = line
+
+    def process_read_file_simple(self):
+        if os.path.isfile(self.csv_input):
+            with open(self.csv_input, 'r') as f:
+                for line in f:
+                    self.process_read_line(line)
+            f.close()
+            print "num of boxes simple", self.num
+            pass
+
+    def process_read_file_predict(self):
+        if os.path.isfile(self.csv_input_predict):
+            with open(self.csv_input_predict, 'r') as f:
+                for line in f:
+                    self.process_read_line(line)
+            f.close()
+            print "num of boxes predict", self.num
+            pass
+
+    def process_read_file_dot(self):
+        if os.path.isfile(self.csv_input_dot):
+            with open(self.csv_input_dot, 'r') as f:
+                for line in f:
+                    self.process_read_line(line)
+            f.close()
+            print "num of boxes dot", self.num
+            pass
+
+    def process_read_file_predict_list(self):
+        if os.path.isfile(self.csv_input_predict_list):
+            with open(self.csv_input_predict_list, 'r') as f:
+                for line in f:
+                    self.process_read_line(line)
+            f.close()
+            print "num of boxes predict list", self.num
+            pass
+
+    def process_read_file_convolution_in_depth(self, num = 1):
+        self.num_chosen = num
+        self.process_read_file_simple()
+        print self.num_chosen
+        ll = loader.Load(self.a, self.picname)
+        return ll.outside_get_pixels_from_dat(self.picname, self.line_chosen)
+        pass
+
