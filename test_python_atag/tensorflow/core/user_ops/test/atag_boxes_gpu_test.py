09aaff61e3bc4259671cfa0e260ff9508e8c48ba
David Liebman <david.c.liebman@gmail.com>
2017-07-27 09:27:40 -0400

    test runs but is hard to read

--- test_python_atag/tensorflow/core/user_ops/test/atag_boxes_gpu_test.py
+++ test_python_atag/tensorflow/core/user_ops/test/atag_boxes_gpu_test.py
@@ -1,33 +1,41 @@
 #!/usr/bin/python
 
 import tensorflow as tf
+import atag_csv_draw_mod as draw
 
-class AssembleBoxesTest(tf.test.TestCase):
-  def testAssembleBoxes(self):
+class AssembleBoxesTest(object):
+  def __init__(self):
+
+    self.d = draw.Read()
+    self.d.process_read_file_predict_list()
+
+  def do_test(self):
+    test = self.d.gpu_test
     assemble_module = tf.load_op_library('./../assemble_boxes_gpu.so')
-    with self.test_session():
+    with tf.Session():
       with tf.device("/gpu:0"):
-      
-          test = [1,1,3,3,1,15]
-          test.extend([4,1,3,3,2,15])
-          test.extend([7,1,3,3,3,15])
-          test.extend([1,4,3,3,4,15])
-          test.extend([4,4,3,3,5,15])
-          test.extend([7,4,3,3,6,15])
-          test.extend([1,7,3,3,7,15])
-          test.extend([4,7,3,3,8,15])
-          test.extend([7,7,3,3,9,15])
-          test.extend([6,9])
+          if len(test) == 0:
+              test = [1,1,3,3,1,15]
+              test.extend([4,1,3,3,2,15])
+              test.extend([7,1,3,3,3,15])
+              test.extend([1,4,3,3,4,15])
+              test.extend([4,4,3,3,5,15])
+              test.extend([7,4,3,3,6,15])
+              test.extend([1,7,3,3,7,15])
+              test.extend([4,7,3,3,8,15])
+              test.extend([7,7,3,3,9,15])
+              test.extend([6,9])
           test = tf.constant(test, dtype=tf.uint16)
           #test = tf.cast(test, dtype=tf.int32)
       print test
       result = assemble_module.assemble_boxes_op(test)
       r = result.eval()
-      for i in range(9):
+      for i in range(len(r) // 6):
           print(r[ i * 6: i * 6 + 6])
           
       #print(r)
       #self.assertAllEqual(result.eval(), test)
 
 if __name__ == "__main__":
-  tf.test.main()
+    a = AssembleBoxesTest()
+    a.do_test()
--- test_python_atag/tensorflow/core/user_ops/test/atag_csv_draw_mod.py
+++ test_python_atag/tensorflow/core/user_ops/test/atag_csv_draw_mod.py
@@ -8,47 +8,50 @@ import atag_csv as enum
 Here we read the csv file that we made and train the models
 '''
 
-class Read( enum.Enum, dim.Dimension) :
-    def __init__(self, atag):
+class Read( enum.Enum) :
+    def __init__(self):
         enum.Enum.__init__(self)
-        dim.Dimension.__init__(self)
+        #dim.Dimension.__init__(self)
 
 
         self.boxlist_r = []
         self.boxlist_g = []
         self.boxlist_b = []
+        self.gpu_test = []
         self.num = 0
 
         self.num_chosen = 0
         self.line_chosen = []
-        self.dim_x = self.DIMENSIONS[self.key][self.COLUMN_XY_CONV][0]
-        self.dim_y = self.DIMENSIONS[self.key][self.COLUMN_XY_CONV][1]
+        #self.dim_x = self.DIMENSIONS[self.key][self.COLUMN_XY_CONV][0]
+        #self.dim_y = self.DIMENSIONS[self.key][self.COLUMN_XY_CONV][1]
         self.img = None
-        self.image_folder = atag.VAR_ROOT_DATABASE
+        #self.image_folder = atag.VAR_ROOT_DATABASE
 
 
-        self.a = atag
-        self.picname = atag.VAR_IMAGE_NAME
-        self.folder = atag.VAR_ROOT_DATABASE
+        #self.a = atag
+        #self.picname = atag.VAR_IMAGE_NAME
+        #self.folder = atag.VAR_ROOT_DATABASE
 
-        if self.picname.startswith(self.folder) :
-            self.picname = self.picname[len(self.folder)+1 : ]
-        print self.picname, "picname"
+        #if self.picname.startswith(self.folder) :
+        #    self.picname = self.picname[len(self.folder)+1 : ]
+        #print self.picname, "picname"
 
-        self.csv_input = atag.VAR_LOCAL_DATABASE + os.sep + atag.VAR_MY_CSV_NAME + ".csv"
-        self.csv_input_predict = atag.VAR_LOCAL_DATABASE + os.sep + "predict" + ".csv"
-        self.csv_input_dot = atag.VAR_LOCAL_DATABASE + os.sep + atag.VAR_MY_CSV_NAME + ".dot.csv"
-        self.csv_input_predict_list = atag.VAR_LOCAL_DATABASE + os.sep + "predict-list" + ".csv"
+        #self.csv_input = atag.VAR_LOCAL_DATABASE + os.sep + atag.VAR_MY_CSV_NAME + ".csv"
+        #self.csv_input_predict = atag.VAR_LOCAL_DATABASE + os.sep + "predict" + ".csv"
+        #self.csv_input_dot = atag.VAR_LOCAL_DATABASE + os.sep + atag.VAR_MY_CSV_NAME + ".dot.csv"
+        self.csv_input_predict_list = "." + os.sep + "predict-list" + ".csv"
 
     def process_read_line(self, line):
         line = line.split(",")
         #print line[self.FILE], "read"
-        if line[self.FILE].endswith(self.picname) :
+        if True :
             self.num = self.num + 1
 
             if line[self.COLOR] == self.RED :
                 self.boxlist_r.append([int(line[self.FACE_X]), int(line[self.FACE_Y]),
                                  int (line[self.FACE_WIDTH]) , int (line[self.FACE_HEIGHT])])
+                self.gpu_test.extend([int(line[self.FACE_X]), int(line[self.FACE_Y]),
+                                 int (line[self.FACE_WIDTH]) , int (line[self.FACE_HEIGHT]), self.num, 15])
             if line[self.COLOR] == self.GREEN:
                 self.boxlist_g.append([int(line[self.FACE_X]), int(line[self.FACE_Y]),
                                        int(line[self.FACE_WIDTH]), int(line[self.FACE_HEIGHT])])
@@ -58,6 +61,7 @@ class Read( enum.Enum, dim.Dimension) :
             if self.num == self.num_chosen:
                 self.line_chosen = line
 
+    '''
     def process_read_file_simple(self):
         if os.path.isfile(self.csv_input):
             with open(self.csv_input, 'r') as f:
@@ -84,16 +88,20 @@ class Read( enum.Enum, dim.Dimension) :
             f.close()
             print "num of boxes dot", self.num
             pass
-
+    '''
+    
     def process_read_file_predict_list(self):
         if os.path.isfile(self.csv_input_predict_list):
+            self.gpu_test = []
             with open(self.csv_input_predict_list, 'r') as f:
                 for line in f:
                     self.process_read_line(line)
             f.close()
             print "num of boxes predict list", self.num
+            self.gpu_test.extend([6,self.num])
             pass
-
+    
+    '''
     def process_read_file_convolution_in_depth(self, num = 1):
         self.num_chosen = num
         self.process_read_file_simple()
@@ -101,4 +109,4 @@ class Read( enum.Enum, dim.Dimension) :
         ll = loader.Load(self.a, self.picname)
         return ll.outside_get_pixels_from_dat(self.picname, self.line_chosen)
         pass
-
+    '''
